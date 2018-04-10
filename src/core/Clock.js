ee531d2b44f24e2b43168dfe4e5401b9d3cb0b02
1 <1429648501@qq.com>
2018-04-10 14:27:09 +0800

    1. 解析Clock函数

--- src/core/Clock.js
+++ src/core/Clock.js
@@ -2,19 +2,36 @@
  * @author alteredq / http://alteredqualia.com/
  */
 
+/**
+ * Clock对象的构造函数.用来记录时间.Clock对象的功能函数采用
+ * 定义构造的函数原型对象来实现
+ * 用法：
+ *      var clock = new Clock(true)
+ *      创建时钟来记录时间，传递参数true，设置自动开始记录
+ *      参数(autostart)为true,自动开始记录,省略该参数也自动开始记录时间,设置为false,穿件时间,但不开始记录.以毫秒为单位,从 1 January 1970 00:00:00 UTC开始.
+ * */
 function Clock( autoStart ) {
 
+    // 自动开始记录时间
 	this.autoStart = ( autoStart !== undefined ) ? autoStart : true;
 
+    // 开始记录的时间截,以毫秒为单位,从 1 January 1970 00:00:00 UTC开始.
 	this.startTime = 0;
+    // 上一次记录时间截.以毫秒为单位,从 1 January 1970 00:00:00 UTC开始.
 	this.oldTime = 0;
+    // 记录当前时间距离上一次记录时间截.以毫秒为单位,从 1 January 1970 00:00:00 UTC开始.
 	this.elapsedTime = 0;
 
+    // 用来跟踪时钟是否在记录时间.
 	this.running = false;
-
 }
+
+/****************************************
+ ****下面是Clock对象提供的功能函数.
+ ****************************************/
 Object.assign( Clock.prototype, {
 
+    // start方法用来开始记录时间,获得开始的时间截.
 	start: function () {
 
 		this.startTime = ( typeof performance === 'undefined' ? Date : performance ).now(); // see #10732
@@ -25,6 +42,7 @@ Object.assign( Clock.prototype, {
 
 	},
 
+    // stop方法用来停止记录时间,获得结束的时间截.
 	stop: function () {
 
 		this.getElapsedTime();
@@ -33,6 +51,7 @@ Object.assign( Clock.prototype, {
 
 	},
 
+    // getElapsedTime方法用来返回从oldTimed到stop之间的时间长度,以秒为单位.
 	getElapsedTime: function () {
 
 		this.getDelta();
@@ -40,6 +59,7 @@ Object.assign( Clock.prototype, {
 
 	},
 
+    // getDelta方法是getElapsedTime方法的实现,具体的算法.
 	getDelta: function () {
 
 		var diff = 0;
@@ -62,9 +82,9 @@ Object.assign( Clock.prototype, {
 
 		}
 
-		return diff;
-
+		return diff; //返回时间长度
 	}
 
 } );
+
 export { Clock };
