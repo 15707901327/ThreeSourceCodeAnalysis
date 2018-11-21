ed9c2e0842227bee0e76f97a32017a9d335cc18c
1 <1429648501@qq.com>
2018-11-21 10:49:58 +0800

    1.解析人物行走的实例 2.添加gui、动画文档

--- example/js/WebGL.js
+++ example/js/WebGL.js
@@ -5,90 +5,84 @@
 
 var WEBGL = {
 
-	isWebGLAvailable: function () {
+  /**
+   * 检测是否支持webgl
+   */
+  isWebGLAvailable: function () {
+    try {
+      var canvas = document.createElement('canvas');
+      return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
+    } catch (e) {
+      return false;
+    }
+  },
 
-		try {
+  /**
+   * 检测是否支持webgl2
+   * @return {boolean}
+   */
+  isWebGL2Available: function () {
+    try {
+      var canvas = document.createElement('canvas');
+      return !!(window.WebGL2RenderingContext && canvas.getContext('webgl2'));
+    } catch (e) {
+      return false;
+    }
+  },
 
-			var canvas = document.createElement( 'canvas' );
-			return !! ( window.WebGLRenderingContext && ( canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) ) );
+  /**
+   * 获取错误信息元素
+   * @return {*|HTMLDivElement}
+   */
+  getWebGLErrorMessage: function () {
+    return this.getErrorMessage(1);
+  },
 
-		} catch ( e ) {
+  getWebGL2ErrorMessage: function () {
 
-			return false;
+    return this.getErrorMessage(2);
 
-		}
+  },
 
-	},
+  /**
+   * 创建错误的标签提示
+   * @param version 版本
+   * @return {HTMLDivElement}
+   */
+  getErrorMessage: function (version) {
 
-	isWebGL2Available: function () {
+    var names = {
+      1: 'WebGL',
+      2: 'WebGL 2'
+    };
 
-		try {
+    var contexts = {
+      1: window.WebGLRenderingContext,
+      2: window.WebGL2RenderingContext
+    };
 
-			var canvas = document.createElement( 'canvas' );
-			return !! ( window.WebGL2RenderingContext && canvas.getContext( 'webgl2' ) );
+    var message = 'Your $0 does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">$1</a>';
 
-		} catch ( e ) {
+    var element = document.createElement('div');
+    element.id = 'webglmessage';
+    element.style.fontFamily = 'monospace';
+    element.style.fontSize = '13px';
+    element.style.fontWeight = 'normal';
+    element.style.textAlign = 'center';
+    element.style.background = '#fff';
+    element.style.color = '#000';
+    element.style.padding = '1.5em';
+    element.style.width = '400px';
+    element.style.margin = '5em auto 0';
 
-			return false;
+    if (contexts[version]) {
+      message = message.replace('$0', 'graphics card');
+    } else {
+      message = message.replace('$0', 'browser');
+    }
 
-		}
-
-	},
-
-	getWebGLErrorMessage: function () {
-
-		return this.getErrorMessage( 1 );
-
-	},
-
-	getWebGL2ErrorMessage: function () {
-
-		return this.getErrorMessage( 2 );
-
-	},
-
-	getErrorMessage: function ( version ) {
-
-		var names = {
-			1: 'WebGL',
-			2: 'WebGL 2'
-		};
-
-		var contexts = {
-			1: window.WebGLRenderingContext,
-			2: window.WebGL2RenderingContext
-		};
-
-		var message = 'Your $0 does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">$1</a>';
-
-		var element = document.createElement( 'div' );
-		element.id = 'webglmessage';
-		element.style.fontFamily = 'monospace';
-		element.style.fontSize = '13px';
-		element.style.fontWeight = 'normal';
-		element.style.textAlign = 'center';
-		element.style.background = '#fff';
-		element.style.color = '#000';
-		element.style.padding = '1.5em';
-		element.style.width = '400px';
-		element.style.margin = '5em auto 0';
-
-		if ( contexts[ version ] ) {
-
-			message = message.replace( '$0', 'graphics card' );
-
-		} else {
-
-			message = message.replace( '$0', 'browser' );
-
-		}
-
-		message = message.replace( '$1', names[ version ] );
-
-		element.innerHTML = message;
-
-		return element;
-
-	}
-
+    message = message.replace('$1', names[version]);
+    element.innerHTML = message;
+    return element;
+  }
 };
