e17cb2052c68001431e02f4b12bcb33c85ae87f0
15707901327 <peng18401590792>
2019-06-04 18:19:20 +0800

    1.添加显示图片

--- examples/js/PGL/home.js
+++ examples/js/PGL/home.js
@@ -74,8 +74,8 @@ var PGL = PGL || {};
         initScene: function () {
             this.scene = new THREE.Scene();
             // this.scene.fog = new THREE.Fog(0xffffff, 300, 1200);
-            // var axes = new THREE.AxesHelper(100);
-            // this.scene.add(axes);
+            var axes = new THREE.AxesHelper(100);
+            this.scene.add(axes);
         },
         initCamera: function () {
             this.camera = new THREE.PerspectiveCamera(45, this.getWidth() / this.getHeight(), 0.1, 30000);
@@ -110,9 +110,9 @@ var PGL = PGL || {};
             //是否自动旋转
             this.orbitControls.autoRotate = false;
             //设置相机距离原点的最远距离
-            this.orbitControls.minDistance = 1;
+            // this.orbitControls.minDistance = 0.1;
             //设置相机距离原点的最远距离
-            this.orbitControls.maxDistance = 30000;
+            // this.orbitControls.maxDistance = 30000;
             //是否开启右键拖拽
             this.orbitControls.enablePan = true;
         },
@@ -121,7 +121,7 @@ var PGL = PGL || {};
             this.container.appendChild(this.stats.dom);
         },
         render: function () {
-            this.mesh.rotateZ(Math.PI/4);
+            // this.mesh.rotateZ(Math.PI / 4);
             this.webGLRenderer.render(this.scene, this.camera);
         },
         initEvent: function () {
@@ -132,89 +132,36 @@ var PGL = PGL || {};
         initObject: function () {
             var bufferGeometry = new THREE.BufferGeometry();
 
-            var positions = new Float32Array([0.0, 0.5, 0, -0.5, -0.5, 0, 0.5, -0.5, 0]);
+            var positions = new Float32Array([
+                -0.5, 0.5, 0.0,
+                -0.5, -0.5, 0.0,
+                0.5, 0.5, 0.0,
+                0.5, -0.5, 0.0
+            ]);
+            var uvs = new Float32Array([
+                0.0, 1.0,
+                0.0, 0.0,
+                1.0, 1.0,
+                1.0, 0.0
+            ]);
             bufferGeometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+            bufferGeometry.addAttribute('uv', new THREE.Float32BufferAttribute(uvs, 2));
 
+            var texture = new THREE.TextureLoader().load( 'textures/land_ocean_ice_cloud_2048.jpg' );
             var meshPhongMaterial = new THREE.MeshPhongMaterial({
-                color: new THREE.Color(1, 0, 0)
+                map:texture
             });
 
             var mesh = new THREE.Mesh(bufferGeometry, meshPhongMaterial);
+            mesh.drawMode = THREE.TriangleStripDrawMode;
             this.mesh = mesh;
             this.scene.add(mesh);
-
-            // // 拉伸体
-            // var arr = [
-            //     [40.52500000002626, 26.459999999966193, 80.99999999999999],
-            //     [36.634000000079624, 26.959999999966424, 80.99999999999999],
-            //     [36.57030044973772, 26.464290099220907, 80.99999999999999],
-            //     [33.701000000006054, 26.833000000102444, 80.99999999999999],
-            //     [33.49407705513509, 25.222725642377508, 80.99999999999999],
-            //     [30.527000000002328, 25.6040000002868, 80.99999999999999],
-            //     [30.276968907796963, 23.535438281022106, 80.99999999999999],
-            //     [23.95100000000447, 24.331999999939416, 80.99999999999999],
-            //     [24.30987265532082, 27.19533831626037, 80.99999999999999],
-            //     [27.11900000006603, 26.919000000225612, 80.99999999999999],
-            //     [27.478000000006052, 29.922999999951895, 80.99999999999999],
-            //     [25.97846627236111, 30.105401669629337, 80.99999999999999],
-            //     [14.201000000002887, 31.538000000174716, 80.99999999999999],
-            //     [14.635000000006519, 34.84600000037118, 80.99999999999999],
-            //     [13.661000000086613, 34.99300000024936, 80.99999999999999],
-            //     [14.096000000015552, 38.44599999999436, 80.99999999999999],
-            //     [3.09800000000298, 39.82500000018561, 80.99999999999999],
-            //     [1.8754457793016917, 30.455517484342494, 80.99999999999999],
-            //     [0.9760000000307336, 30.54000000003669, 80.99999999999999],
-            //     [0.0, 22.901000000071992, 80.99999999999999],
-            //     [1.4840000000543891, 22.672000000256812, 80.99999999999999],
-            //     [1.0393455140318721, 19.130212833174085, 80.99999999999999],
-            //     [0.8220000000485219, 17.399000000206986, 80.99999999999999],
-            //     [4.726000000029523, 16.813000000083587, 80.99999999999999],
-            //     [5.318883500185236, 20.76285867579556, 80.99999999999999],
-            //     [6.697000000043865, 20.55600000033318, 80.99999999999999],
-            //     [6.795000000045448, 21.235000000334857, 80.99999999999999],
-            //     [7.888389264470338, 21.100516625218138, 80.99999999999999],
-            //     [6.99499999999823, 13.83700000029439, 80.99999999999999],
-            //     [11.987000000077671, 13.2230000002339, 80.99999999999999],
-            //     [12.453684766656252, 17.017283964904863, 80.99999999999999],
-            //     [14.578000000100582, 16.756000000053458, 80.99999999999999],
-            //     [14.805772755992132, 18.607859280429455, 80.99999999999999],
-            //     [15.951000000000745, 18.46700000017886, 80.99999999999999],
-            //     [14.642000000110547, 7.996000000278838, 80.99999999999999],
-            //     [20.0200000000176, 7.3760000001638195, 80.99999999999999],
-            //     [20.468325108641665, 11.264858764131413, 80.99999999999999],
-            //     [20.736000000035204, 11.23400000017425, 80.99999999999999],
-            //     [21.682624788611566, 11.1345270154987, 80.99999999999999],
-            //     [20.90800000005681, 4.473000000232784, 80.99999999999999],
-            //     [23.897132867132964, 4.056048264264967, 80.99999999999999],
-            //     [23.775000000021418, 3.2020000000472644, 80.99999999999999],
-            //     [27.97708259022031, 2.614745110275969, 80.99999999999999],
-            //     [28.964772880189585, 9.682129723483975, 80.99999999999999],
-            //     [29.977347049630804, 9.54408225324247, 80.99999999999999],
-            //     [29.668000000067984, 6.772000000349944, 80.99999999999999],
-            //     [31.12813672446748, 6.625312284104712, 80.99999999999999],
-            //     [30.438000000085495, 0.8340000002640299, 80.99999999999999],
-            //     [37.11600000003455, 0.0, 80.99999999999999]
-            // ];
-            // var extrudeMesh = new THREE.ExtrudeMesh();
-            // console.log(extrudeMesh.getArrLength(arr));
-            // var geometry = extrudeMesh.createExtrudeBufferGeometry(arr, 100);
-            // var texture = new THREE.TextureLoader().load('textures/cube/skybox/nx.jpg');
-            // texture.wrapS = THREE.RepeatWrapping;
-            // texture.wrapT = THREE.RepeatWrapping;
-            // texture.repeat.set( 1, 1 );
-            //
-            // var material = new THREE.MeshPhongMaterial({
-            //     map: texture
-            // });
-            // var mesh = new THREE.Mesh(geometry, material);
-            // this.scene.add(mesh);
-            // console.log(mesh);
         },
         /**
          * 添加天空盒子
          */
         initSkyBox: function () {
-            let texture = new THREE.TextureLoader().load('../statics/image/skybox/sky.png');
+            let texture = new THREE.TextureLoader().load('textures/skybox/sky.png');
             let sphereGeometry = new THREE.SphereGeometry(700, 50, 50);
             sphereGeometry.scale(-1, 1, 1);
             let sphereMaterial = new THREE.MeshBasicMaterial({
@@ -259,7 +206,7 @@ var PGL = PGL || {};
 $(function () {
     var scene3D = new PGL.scene3D({}, {
         container: document.getElementById("container"),
-        enabledSkyBox: false
+        enabledSkyBox: true
     });
     scene3D.init();
 });
--- src/textures/Texture.js
+++ src/textures/Texture.js
@@ -5,11 +5,21 @@
  */
 
 import { EventDispatcher } from '../core/EventDispatcher.js';
-import { UVMapping } from '../constants.js';
-import { MirroredRepeatWrapping, ClampToEdgeWrapping, RepeatWrapping, LinearEncoding, UnsignedByteType, RGBAFormat, LinearMipMapLinearFilter, LinearFilter } from '../constants.js';
+import {
+	MirroredRepeatWrapping,
+	ClampToEdgeWrapping,
+	RepeatWrapping,
+	LinearEncoding,
+	UnsignedByteType,
+	RGBAFormat,
+	LinearMipMapLinearFilter,
+	LinearFilter,
+	UVMapping
+} from '../constants.js';
 import { _Math } from '../math/Math.js';
 import { Vector2 } from '../math/Vector2.js';
 import { Matrix3 } from '../math/Matrix3.js';
+import { ImageUtils } from '../extras/ImageUtils.js';
 
 var textureId = 0;
 
@@ -53,7 +63,7 @@ function Texture( image, mapping, wrapS, wrapT, magFilter, minFilter, format, ty
 	// Values of encoding !== THREE.LinearEncoding only supported on map, envMap and emissiveMap.
 	//
 	// Also changing the encoding after already used by a Material will not automatically make the Material
-	// update.  You need to explicitly call Material.needsUpdate to trigger it to recompile.
+	// update. You need to explicitly call Material.needsUpdate to trigger it to recompile.
 	this.encoding = encoding !== undefined ? encoding : LinearEncoding;
 
 	this.version = 0;
@@ -130,46 +140,6 @@ Texture.prototype = Object.assign( Object.create( EventDispatcher.prototype ), {
 
 		}
 
-		function getDataURL( image ) {
-
-			var canvas;
-
-			if ( image instanceof HTMLCanvasElement ) {
-
-				canvas = image;
-
-			} else {
-
-				canvas = document.createElementNS( 'http://www.w3.org/1999/xhtml', 'canvas' );
-				canvas.width = image.width;
-				canvas.height = image.height;
-
-				var context = canvas.getContext( '2d' );
-
-				if ( image instanceof ImageData ) {
-
-					context.putImageData( image, 0, 0 );
-
-				} else {
-
-					context.drawImage( image, 0, 0, image.width, image.height );
-
-				}
-
-			}
-
-			if ( canvas.width > 2048 || canvas.height > 2048 ) {
-
-				return canvas.toDataURL( 'image/jpeg', 0.6 );
-
-			} else {
-
-				return canvas.toDataURL( 'image/png' );
-
-			}
-
-		}
-
 		var output = {
 
 			metadata: {
@@ -191,11 +161,17 @@ Texture.prototype = Object.assign( Object.create( EventDispatcher.prototype ), {
 			wrap: [ this.wrapS, this.wrapT ],
 
 			format: this.format,
+			type: this.type,
+			encoding: this.encoding,
+
 			minFilter: this.minFilter,
 			magFilter: this.magFilter,
 			anisotropy: this.anisotropy,
 
-			flipY: this.flipY
+			flipY: this.flipY,
+
+			premultiplyAlpha: this.premultiplyAlpha,
+			unpackAlignment: this.unpackAlignment
 
 		};
 
@@ -223,7 +199,7 @@ Texture.prototype = Object.assign( Object.create( EventDispatcher.prototype ), {
 
 					for ( var i = 0, l = image.length; i < l; i ++ ) {
 
-						url.push( getDataURL( image[ i ] ) );
+						url.push( ImageUtils.getDataURL( image[ i ] ) );
 
 					}
 
@@ -231,7 +207,7 @@ Texture.prototype = Object.assign( Object.create( EventDispatcher.prototype ), {
 
 					// process single image
 
-					url = getDataURL( image );
+					url = ImageUtils.getDataURL( image );
 
 				}
 
@@ -264,7 +240,7 @@ Texture.prototype = Object.assign( Object.create( EventDispatcher.prototype ), {
 
 	transformUv: function ( uv ) {
 
-		if ( this.mapping !== UVMapping ) return;
+		if ( this.mapping !== UVMapping ) return uv;
 
 		uv.applyMatrix3( this.matrix );
 
@@ -335,6 +311,8 @@ Texture.prototype = Object.assign( Object.create( EventDispatcher.prototype ), {
 			uv.y = 1 - uv.y;
 
 		}
+
+		return uv;
 
 	}
 
