bc737e415047e5591dc5b7bdc5fcc09180f5d44c
peng <1429648501@qq.com>
2019-09-24 13:47:02 +0800

    1.blur

--- examples/Blur/Blur.js
+++ examples/Blur/Blur.js
@@ -1,5 +1,6 @@
-let Blur = function() {
+import * as THREE from "../../build/three_r108.module.js";
 
+let Blur = function() {
 };
 Blur.GaussianShader = {
   uniforms: {
@@ -45,6 +46,56 @@ Blur.GaussianShader = {
     '}'
   ].join('\n')
 };
+Blur.Gaussian2Shader = {
+  uniforms: {
+    h: {value: null},
+    v: {value: null},
+    tDiffuse: {value: null}
+  },
+  vertexShader: [
+    'varying vec2 v_texCoord;' +
+    ' void main(){' +
+    ' v_texCoord = uv;' +
+    ' gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );' +
+    '}'
+  ].join('\n'),
+  fragmentShader: [
+    'precision mediump float;' +
+    'uniform sampler2D tDiffuse;' +
+    'uniform float h;' +
+    'uniform float v;' +
+    'varying vec2 v_texCoord;' +
+    'void main(){' +
+    ' const float GlowRange = 1.0;' + // 模糊半径
+    ' const float samplerPre = 3.0;' +
+    ' float count = 0.0;' +
+    ' const float range = GlowRange * 2.0;' +
+    ' vec4 clraverge = vec4( 0.0 );' +
+    '  for( float i = -range ; i <= range ; i += samplerPre ){' +
+    '    for( float j = -range ; j <= range ; j += samplerPre ){' +
+    '      float nx = j;' +
+    '      float ny = i;' +
+    '      float q = range / 1.75;' +
+    '      float gr = (1.0 / ( 2.0 * 3.14159 * q * q)) * exp( -( nx * nx + ny * ny) / ( 2.0 * q * q)) * 9.0;' +
+    '      vec2 samplerTexCoord = vec2( v_texCoord.x + j * h , v_texCoord.y + i * v );' +
+    '      if( samplerTexCoord.x < 0.0)' +
+    '        samplerTexCoord.x = -samplerTexCoord.x;' +
+    '      else if(samplerTexCoord.x > 1.0)' +
+    '        samplerTexCoord.x = 2.0 - samplerTexCoord.x;' +
+    '      if(samplerTexCoord.y < 0.0)' +
+    '        samplerTexCoord.y = -samplerTexCoord.y;' +
+    '      else if(samplerTexCoord.y > 1.0)' +
+    '        samplerTexCoord.y = 2.0 - samplerTexCoord.y;' +
+    '      vec4 tc = texture2D( tDiffuse, samplerTexCoord );' +
+    '      clraverge += tc * gr;' +
+    '      count += 1.0;' +
+    '    }' +
+    '  }' +
+    ' gl_FragColor = clraverge;' +
+    '}'
+  ].join('\n')
+};
+
 Blur.MotionShader = {
   uniforms: {
     GlowRange: {value: null},
@@ -91,83 +142,92 @@ Blur.MotionShader = {
   ].join('\n')
 };
 
-Blur.CommonShader = {
+Blur.GeneralShader = {
   uniforms: {
     h: {value: null},
     v: {value: null},
     tDiffuse: {value: null}
   },
   vertexShader: [
-    'varying vec2 vUv;' +
+    'varying vec2 v_texCoord;' +
     ' void main(){' +
-    ' vUv = uv;' +
+    ' v_texCoord = uv;' +
     ' gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );' +
     '}'
   ].join('\n'),
   fragmentShader: [
-    '#ifdef GL_ES' +
     'precision mediump float;' +
-    '#endif ' +
-    'uniform float mode;  // 0普通模糊 1高斯模糊 2动感模糊' +
-    'uniform vec2 resolution;' +
-    'uniform float GlowRange; // 模糊半径' +
-    'uniform float GlowExpand; // 动感模糊角度' +
-    'varying vec4 v_fragmentColor;' +
+    'uniform sampler2D tDiffuse;' +
+    'uniform float h;' +
+    'uniform float v;' +
     'varying vec2 v_texCoord;' +
     'void main(){' +
-    ' vec4 clraverge = vec4(0,0,0,0);' +
-    ' if( GlowRange > 0.0 ){' +
-    '   if(mode == 2){' +
-    '     float samplerPre = 1;' +
-    '     float range = GlowRange * 3;' +
-    '     float rad = GlowExpand;' +
-    '     for( float j = 1; j <= range ; j += samplerPre ){' +
-    '       float dx = 0.002 * cos(rad);' +
-    '       float dy = 0.002 * sin(rad);' +
-    '       vec2 samplerTexCoord = vec2( v_texCoord.x + j * dx, v_texCoord.y + j * dy);' +
-    '       vec2 samplerTexCoord1= vec2( v_texCoord.x - j * dx, v_texCoord.y - j * dy);' +
-    '       if( samplerTexCoord.x < 0.0 || samplerTexCoord.x > 1.0 || samplerTexCoord1.x < 0.0 || samplerTexCoord1.x > 1.0|| samplerTexCoord.y < 0.0 || samplerTexCoord.y > 1.0 ||samplerTexCoord1.y < 0.0 || samplerTexCoord1.y > 1.0){' +
-    '         continue;' +
-    '       }' +
-    '       vec4 tc = texture2D( CC_Texture0, samplerTexCoord );' +
-    '       vec4 tc1= texture2D( CC_Texture0, samplerTexCoord1 );' +
-    '       clraverge += tc;' +
-    '       clraverge += tc1;' +
-    '     }' +
-    '     clraverge /= (range * 2);' +
-    '   }else{' +
-    '     float samplerPre = 3.0;' +
-    '     float radiusX = 1.0 / TextureSize.x;' +
-    '     float radiusY = 1.0 / TextureSize.y;' +
-    '     float count = 0.0;' +
-    '     float range = GlowRange * 2.0;' +
-    '     for( float i = -range ; i <= range ; i += samplerPre ){' +
-    '       for( float j = -range ; j <= range ; j += samplerPre ){' +
-    '         float nx = j;' +
-    '         float ny = i;' +
-    '         float q = range / 1.75;' +
-    '         float gr = (1.0/(2*3.14159*q*q))* exp(-(nx*nx+ny*ny)/(2*q*q))*9.0;' +
-    '         vec2 samplerTexCoord = vec2( v_texCoord.x + j * radiusX , v_texCoord.y + i * radiusY );' +
-    '         if( samplerTexCoord.x < 0.0)' +
-    '           samplerTexCoord.x=-samplerTexCoord.x;' +
-    '         else if(samplerTexCoord.x > 1.0)' +
-    '           samplerTexCoord.x = 2-samplerTexCoord.x;' +
-    '         if(samplerTexCoord.y < 0.0)' +
-    '           samplerTexCoord.y=-samplerTexCoord.y;' +
-    '         else if(samplerTexCoord.y > 1.0)' +
-    '           samplerTexCoord.y = 2-samplerTexCoord.y;' +
-    '         vec4 tc = texture2D( CC_Texture0, samplerTexCoord );' +
-    '         if(mode == 0)' +
-    '           clraverge + =tc;' +
-    '         else if(mode == 1)' +
-    '           clraverge += tc * gr;' +
-    '         count+=1;' +
-    '       }' +
-    '     }' +
-    '     if(mode==0)' +
-    '       clraverge /= count;' +
+    ' const float GlowRange = 1.0;' + // 模糊半径
+    ' const float samplerPre = 3.0;' +
+    ' float count = 0.0;' +
+    ' const float range = GlowRange * 2.0;' +
+    ' vec4 clraverge = vec4( 0.0 );' +
+    ' for( float i = -range ; i <= range ; i += samplerPre ){' +
+    '   for( float j = -range ; j <= range ; j += samplerPre ){' +
+    '     vec2 samplerTexCoord = vec2( v_texCoord.x + j * h , v_texCoord.y + i * v );' +
+    '     if( samplerTexCoord.x < 0.0)' +
+    '       samplerTexCoord.x = -samplerTexCoord.x;' +
+    '     else if(samplerTexCoord.x > 1.0)' +
+    '       samplerTexCoord.x = 2.0 - samplerTexCoord.x;' +
+    '     if(samplerTexCoord.y < 0.0)' +
+    '       samplerTexCoord.y = -samplerTexCoord.y;' +
+    '     else if(samplerTexCoord.y > 1.0)' +
+    '       samplerTexCoord.y = 2.0 - samplerTexCoord.y;' +
+    '     vec4 tc = texture2D( tDiffuse, samplerTexCoord );' +
+    '     clraverge += tc;' +
+    '     count += 1.0;' +
     '   }' +
     ' }' +
+    ' clraverge /= count;' +
+    ' gl_FragColor = clraverge;' +
+    '}'
+  ].join('\n')
+};
+
+Blur.RadialShader = {
+  uniforms: {
+    centerpos: {value: new THREE.Vector2(0.5, 0.5)},
+    tDiffuse: {value: null}
+  },
+  vertexShader: [
+    'varying vec2 v_texCoord;' +
+    ' void main(){' +
+    ' v_texCoord = uv;' +
+    ' gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );' +
+    '}'
+  ].join('\n'),
+  fragmentShader: [
+    'uniform sampler2D tDiffuse;' +
+    'uniform vec2 centerpos;' + // 径向中心点
+    'varying vec2 v_texCoord;' +
+    'void main(){' +
+    ' vec4 clraverge = vec4( 0.0 );' +
+    ' float count = 0.0, x1, y1;' +
+    ' vec2 cpos = centerpos;' +
+    ' const float range = 50.0;' + // 径向范围
+    ' for( float j = 1.0; j <= range ; j += 1.0 ){' +
+    '   if(cpos.x - v_texCoord.x == 0.0){' +
+    '     x1 = v_texCoord.x;' +
+    '     y1= v_texCoord.y + (cpos.y - v_texCoord.y) * j / (6.0 * range);' +
+    '   }' +
+    '   else{' +
+    '     float k = (cpos.y - v_texCoord.y) / ( cpos.x - v_texCoord.x );' +
+    '     x1= v_texCoord.x + (cpos.x - v_texCoord.x) * j / 200.0;' +
+    '     if( ( cpos.x - v_texCoord.x ) * ( cpos.x - x1) < 0.0 )' +
+    '        x1 = cpos.x;' +
+    '     y1 = cpos.y - cpos.x * k + k * x1;' +
+    '     if( x1<0.0 || y1<0.0 || x1>1.0 || y1>1.0 )' +
+    '       continue;' +
+    '   }' +
+    '   clraverge += texture2D( tDiffuse, vec2(x1,y1) );' +
+    '   count += 1.0;' +
+    ' }' +
+    ' clraverge /= count;' +
     ' gl_FragColor = clraverge;' +
     '}'
   ].join('\n')
